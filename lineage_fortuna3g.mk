4e0d566feb9c1af194791248854626eeb31f27ea
Phan Duc <38982972+Gumball1506@users.noreply.github.com>
2019-06-29 13:24:58 +0700

    Update and rename aosp_fortuna3g.mk to lineage_fortuna3g.mk

--- lineage_fortuna3g.mk
+++ lineage_fortuna3g.mk
@@ -1,10 +1,10 @@
 # Inherit from common
-$(call inherit-product, device/samsung/gprimelte-common/aosp.mk)
+$(call inherit-product, device/samsung/gprimelte-common/lineage.mk)
 $(call inherit-product, device/samsung/fortuna3g/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := fortuna3g
-PRODUCT_NAME := aosp_fortuna3g
+PRODUCT_NAME := lineage_fortuna3g
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
